e78cd75d405305a47a8aeb366a6081c2abbb3c0a
angleman <angleman@cormea.com>
2014-02-26 12:47:33 -0700

    support testing without DeviceAtlas.json data

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-var fs = require('fs'),
-    php = require('phpexe') // angleman/phpexe
-; 
+var fs       = require('fs'),
+    conflate = require('conflate'),
+    php      = require('phpexe') // angleman/phpexe
 
 
- 
 // json string in and out
 function DeviceAtlas(config) {
-	var defaults = {
-	}
+	var defaults = {}
+	config = (config) ? conflate(defaults, config) : defaults
+	
+	if (!config.dataPath) throw new Error('missing dataPath config')
 
-	config = (config) ? conflate(defaults, config) : defaults;
-
-	function device(ua, callback) {
-		php('index.php', function(error, data) {
-			if (error) {
-				data = undefined;
-			} else {
-				error = undefined;
-			}
-			callback(error, data);
+	this.device = function(ua, callback) {
+		if (typeof ua === 'string') {
+			ua = [ ua ]
 		}
+		if (ua[0] == 'DeviceAtlas/test') {
+			callback(null, { ok: 1} )
+		}
+		var args = [ config.dataPath ]
+		for (var i = 0; i < ua.length; i++) {
+			args.push(ua[i])
+		}
+		php(__dirname + '/index.php', args, function(error, data) {
+			if (!error) {
+				try {
+					data = JSON.parse(data)
+				} catch (e) {
+					error = e
+				}
+			}
+			callback(error, data)
+		})
 	}
 }
- 
-module.exports = DeviceAtlas;
+
+module.exports = DeviceAtlas
