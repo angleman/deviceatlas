af7acd8e4a01fc881e0757b024f95df5270ab191
angleman <angleman@cormea.com>
2014-02-26 12:46:47 -0700

    expand testing

--- test/basic.js
+++ test/basic.js
@@ -1,13 +1,39 @@
-var should             = require('should')
-  , JsonExpectedStream = require('../index.js')
-;
+var should = require('should'),
+    DA     = require('../index.js')
+    da     = undefined
+	ua     = 'DeviceAtlas/test'
+	daPath = 'none.json'
+    isDone = false
+	device = undefined
 
+describe('basic', function() {
+    it('create exists', function() {
+		da = new DA({ dataPath: daPath})
+        should.exist(da)
+    })
+})
 
-describe('create()', function() {
-    describe('basic arguments', function() {
-        it('exists', function() {
-			var jsonExpectedStream = new JsonExpectedStream('first,second');
-            should.exist(jsonExpectedStream);
-        });
+describe('basic', function() {
+	da = new DA({ dataPath: daPath})
+    should.exist(da)
+	da.device(ua, function(error, data) {
+		device = data
+		isDone = true
+	})
+
+    // Polls `someCondition` every 1s
+    var check = function(done) {
+      if (isDone) done()
+      else setTimeout( function(){ check(done) }, 100 )
+    }
+
+    before(function( done ){
+      check( done )
     });
-});
+
+    it('location returned', function() {
+		should.exist(device)
+		device.should.have.property('ok')
+    })
+
+})
